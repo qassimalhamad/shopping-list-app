9b9fd11cafb0988ebbd49376520384b1195b47fa
Qasim <qalhamad33@gmail.com>
2024-08-04 12:24:00 +0300

    Shopping List

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+const App = () => {
+
+  const items = [
+    { name: 'Milk', purchased: true },
+    { name: 'Bread', purchased: false },
+    { name: 'Eggs', purchased: true },
+  ];
+
+  return (
+    <>
+    <h1>Shopping List </h1>
+    <ul>
+      {items.map((item , index) =>(
+        <li key={index} className= {item.purchased ? 'completed' : 'not-completed'} >
+          {item.name}
+        </li>
+      ))}
+    </ul>
+    
+    
+    
+    </>
+  );
+}
+
+export default App
